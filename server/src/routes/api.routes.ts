9457b8ed882c9df78d575a126812c7edfdf7498f
YuraShum <91155294+YuraShum@users.noreply.github.com>
2024-12-13 14:32:15 +0200

    feat: created user routes using controllers and created basic note routes

--- server/src/routes/api.routes.ts
+++ server/src/routes/api.routes.ts
@@ -1,10 +1,15 @@
 import express from 'express'
 import path from 'path'
+import userRoutes from './user.routes'
+import noteRoutes from './note.routes'
 
 const router = express.Router()
 
 router.get('^/$|/index(.html)?', (_, res) => {
     res.sendFile(path.join(__dirname, '..', 'views', 'index.html'))
 })
+
+router.use('/users', userRoutes)
+router.use('/notes', noteRoutes)
 
 export default router
---
+++ server/src/routes/user.routes.ts
@@ -0,0 +1,26 @@
+import express from 'express'
+import { userController } from '../controllers'
+
+const router = express.Router()
+
+router.get(
+    '/',
+    userController.getAllUsers
+)
+
+router.post(
+    '/',
+    userController.createNewUser
+)
+
+router.patch(
+    '/',
+    userController.updateUser
+)
+
+router.delete(
+    '/',
+    userController.deleteUser
+)
+router
+export default router
