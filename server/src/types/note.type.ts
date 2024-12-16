a514ae6dc0ca2a758d863cca36ed5fb6d7a9dfeb
YuraShum <91155294+YuraShum@users.noreply.github.com>
2024-12-16 15:18:48 +0200

    feat: create note types

---
+++ server/src/types/note.type.ts
@@ -0,0 +1,17 @@
+import { Response } from "express";
+import { IErrorResponse, INote } from "../interfaces";
+
+export type GetAllNoteResponse = Response<INote[] | IErrorResponse>;
+
+export type NoteResponse = Response<INote | IErrorResponse>;
+
+export type DeleteNoteResponse = Response<void | IErrorResponse>;
+
+export type CreateNoteBody =  Pick<INote, 'title' | 'text' | 'user' >
+
+export type UpdateNoteBody = {
+    user: string,
+    title: string,
+    text: string,
+    completed: boolean
+}
