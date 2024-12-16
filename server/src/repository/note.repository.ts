ba80741b3affb8478013f1fd2ac488986b3e3ad1
YuraShum <91155294+YuraShum@users.noreply.github.com>
2024-12-16 15:17:26 +0200

    feat: create note repository

---
+++ server/src/repository/note.repository.ts
@@ -0,0 +1,45 @@
+import { INote, INoteRepository } from "../interfaces";
+import { noteModel } from "../models";
+import { CreateNoteBody, UpdateNoteBody } from "../types";
+
+export class NoteRepository implements INoteRepository {
+
+    async getAll(): Promise<INote[] | null> {
+        return noteModel.find().exec()
+    }
+    //!! Перевірити коректність роботи
+    async getAllUserNotes(userId: string): Promise<INote[] | null> {
+        return noteModel.find({user: userId}).exec();
+    }
+
+    async getNoteById(noteId: string): Promise<INote | null> {
+        return noteModel.findById({ id: noteId }).exec()
+    }
+
+    async createNewNote(noteBody: CreateNoteBody): Promise<INote | null> {
+        return noteModel.create(noteBody);
+    }
+
+    async checkDuplicateNote(title: string): Promise<INote | null> {
+        return noteModel.findOne({ title }).exec();
+    }
+
+    async updateNote(noteId: string, updateNoteBody: UpdateNoteBody): Promise<INote | null> {
+        const updateNote = await noteModel.findByIdAndUpdate(
+            { _id: noteId },
+            updateNoteBody,
+            {
+                new: true,
+                lean: true,
+            }
+        ).exec()
+        return updateNote as INote | null;
+    }
+
+    async deleteNote(noteId: string): Promise<void> {
+        await noteModel.findByIdAndDelete({_id: noteId}).exec();
+    }
+
+}
+
+export const noteRepository = new NoteRepository()
