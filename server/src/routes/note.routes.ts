cd8536a844bad6cb94743c7824a77d7d0c2cc100
YuraShum <91155294+YuraShum@users.noreply.github.com>
2024-12-17 13:57:31 +0200

    feat: created note routes endpoints

--- server/src/routes/note.routes.ts
+++ server/src/routes/note.routes.ts
@@ -1,5 +1,15 @@
 import express from 'express';
 
+import { noteController } from '../controllers';
+
 const router = express.Router();
+
+router.get('/', noteController.getAllNotes);
+
+router.post('/', noteController.createNewNote);
+
+router.patch('/', noteController.updateNote);
+
+router.delete('/', noteController.deleteNote);
 
 export default router;
